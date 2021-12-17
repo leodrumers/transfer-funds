b2ac289533c36db50eff99db8d03d7c2b1e8441a
Leandro Ortiz <dreamcode@MacBook-Pro-de-Leandro.local>
2021-12-17 15:45:52 -0500

    Initial commit

---
+++ src/main/java/com/example/funds/transfer/entity/Account.java
@@ -0,0 +1,7 @@
+package com.example.funds.transfer.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Account {
+}
---
+++ src/test/java/com/example/funds/transfer/FundsTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funds.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundsTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
