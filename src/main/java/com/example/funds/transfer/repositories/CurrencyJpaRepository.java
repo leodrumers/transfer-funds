1b7d67b2456d12e9dff7f444199ebbc5309fe4a8
leandro.ortiz <leodrumers@gmail.com>
2021-12-22 19:02:17 -0500

    Unused classes and methods deleted

--- src/main/java/com/example/funds/transfer/repositories/CurrencyJpaRepository.java
+++ src/main/java/com/example/funds/transfer/repositories/CurrencyJpaRepository.java
@@ -4,6 +4,5 @@ import com.example.funds.transfer.entity.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurrencyJpaRepository extends JpaRepository<Currency, Long> {
-    Currency findByCurrencyEquals(String currency);
     Currency findTopByCurrencyEquals(String currency);
 }
--- src/main/java/com/example/funds/transfer/repositories/TransferHistoryRepository.java
+++ src/main/java/com/example/funds/transfer/repositories/TransferHistoryRepository.java
@@ -4,12 +4,10 @@ import com.example.funds.transfer.entity.TransferHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 
 public interface TransferHistoryRepository extends JpaRepository<TransferHistory, Long> {
     Integer countTransferHistoryByOriginAccountAndTransferDateBeforeAndTransferDateAfterAndStatusEquals(
             Long accountId, LocalDateTime before, LocalDateTime after, String status
     );
-    List<TransferHistory> findAllByOriginAccountEquals(Long accountId);
 }
